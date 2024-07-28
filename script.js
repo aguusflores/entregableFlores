5ba0327e907d308f8d4f8a4ed791580d14dd53e5
aguusflores <floresagustina1@gmail.com>
2024-07-28 18:05:49 -0300

    reentrega uno

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ function convertir(moneda, pesos) {
 }
 
 for (let i = 0; i < 3; i++) { 
-    let moneda = prompt("¿A qué moneda querés convertir? (dolar, euros, reales)").toLowerCase();
+    let moneda = prompt("¿A qué moneda querés convertir? (dolar, euros, reales)");
     let pesos = parseInt(prompt("Introduce la cantidad de pesos argentinos"));
     let resultado = convertir(moneda, pesos);
 
     if (moneda === "dolar" || moneda === "euros" || moneda === "reales") {
-        alert("La cantidad de " + pesos + " pesos argentinos es igual a " + resultado + " " + moneda + "(s)");
+        alert("La cantidad de " + pesos + " pesos argentinos es igual a " + resultado + " " + moneda + " en el mercado");
     } else {
         alert("Moneda invalida, seleccione otra");
     }
